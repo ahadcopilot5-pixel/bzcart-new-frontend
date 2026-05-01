e29a36b51286bf5cd4f68480cd618ffd99ea89ae
Ahad Qureshi <ahadqureshi16756@gmail.com>
2026-05-01 12:06:05 +0500

    Show city in testimonial location chip, update layout to match design

--- src/components/home/ClientTestimonials.jsx
+++ src/components/home/ClientTestimonials.jsx
@@ -106,16 +106,14 @@ const ClientTestimonials = () => {
                 <svg className="w-4 h-4 text-orange-500 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
-                {currentTestimonial.name}
+                {currentTestimonial.city || currentTestimonial.name}
               </div>
-              {currentTestimonial.role && (
-                <div className="flex items-center gap-1 text-orange-500 text-xs font-semibold">
-                  {currentTestimonial.role}
-                  <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                  </svg>
-                </div>
-              )}
+              <div className="flex items-center gap-1 text-orange-500 text-xs font-semibold">
+                {currentTestimonial.role || "Verified Customer"}
+                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </div>
             </div>
 
             {/* Rating */}
